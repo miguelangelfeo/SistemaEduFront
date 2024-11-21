b34b5afe2e4ed6590152c0838850ed3bcf6d456c
Miguel Angel Feo Merchan <mfeomerchan@gmail.com>
2024-11-20 19:03:20 -0500

    agrega funciones de asignaturas

--- src/pages/HomeSubject.js
+++ src/pages/HomeSubject.js
@@ -1,25 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
 
 export default function Home() {
-
     const [subjects, setSubjects] = useState([]);
-    const { id } = useParams();
+    const [expandedSubjectId, setExpandedSubjectId] = useState(null);  // Estado para controlar la asignatura expandida
 
     useEffect(() => {
         loadSubjects();
     }, []);
 
     const loadSubjects = async () => {
-        const result = await axios.get("http://localhost:8083/asignaturas-bd");
+        const result = await axios.get("http://localhost:8083/asignaturas");
         setSubjects(result.data);
     };
 
     const deleteSubject = async (id) => {
         await axios.delete(`http://localhost:8083/asignaturaEliminada/${id}`);
         loadSubjects();
-    }
+    };
+
+    const toggleStudents = (id) => {
+        // Cambiar el estado para expandir o contraer los estudiantes de la asignatura
+        setExpandedSubjectId(expandedSubjectId === id ? null : id);
+    };
 
     return (
         <div className='container'>
@@ -29,29 +33,46 @@ export default function Home() {
                         <tr>
                             <th scope="col">Id</th>
                             <th scope="col">Nombre de asignatura</th>
+                            <th scope="col">Descripcion</th>
                             <th scope="col">Creditos</th>
-                            <th scope="col">acciones</th>
+                            <th scope="col">Profesor</th>
+                            <th scope="col">Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             subjects.map((subject, index) => (
-                                <tr>
-                                    <th scope="row" key={index}>
-                                        {index + 1}
-                                    </th>
+                                <React.Fragment key={index}>
+                                    <tr>
+                                        <th scope="row">{index + 1}</th>
+                                        <td>{subject.nombre}</td>
+                                        <td>{subject.descripcion}</td>
+                                        <td>{subject.creditos}</td>
+                                        <td>{subject.profesor ? subject.profesor.nombre : 'No asignado'}</td>  {/* Mostrar el nombre del profesor */}
+                                        <td>
+                                            <button className='btn btn-danger mx-2' onClick={() => deleteSubject(subject.id)}>
+                                                Eliminar
+                                            </button>
+                                            <button className='btn btn-info mx-2' onClick={() => toggleStudents(subject.id)}>
+                                                {expandedSubjectId === subject.id ? 'Ocultar Estudiantes' : 'Ver Estudiantes'}
+                                            </button>
+                                        </td>
+                                    </tr>
 
-                                    <td>{subject.nombre}</td>
-                                    <td>{subject.creditos}</td>
-                                    <td>
-                                        <Link className='btn btn-outline-primary mx-2'
-                                            to={`/editsubject/${subject.id}`}
-                                        >Editar</Link>
-                                        <button className='btn btn-danger mx-2' onClick={() => deleteSubject(subject.id)}>
-                                            Eliminar</button>
-                                    </td>
-
-                                </tr>
+                                    {expandedSubjectId === subject.id && (
+                                        <tr>
+                                            <td colSpan="5">
+                                                <ul>
+                                                    {subject.estudiantes && subject.estudiantes.map((student, studentIndex) => (
+                                                        <li key={studentIndex}>
+                                                            {student.nombre}
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </td>
+                                        </tr>
+                                    )}
+                                </React.Fragment>
                             ))
                         }
                     </tbody>
@@ -59,5 +80,5 @@ export default function Home() {
             </div>
             <Link className='btn btn-primary' to='/'>Volver al inicio</Link>
         </div>
-    )
+    );
 }
